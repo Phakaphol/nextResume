3870a7a33a861c698fd6a7105d8e0fb75d0120b2
Clean456 <cleanfibo@gmail.com>
2022-10-09 20:22:12 +0700

    ssr4

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,10 +16,7 @@ export default function Index({}: Props) {
   
   return (
     <div>
-      <Head>
-       <title>My page title</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
+      
         <div id="loader">
             <div id="shadow"></div>
             <div id="box"></div>
