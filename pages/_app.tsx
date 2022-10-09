d6ce78bdd7bc5438908831deca0e7176aa58dfde
Clean456 <cleanfibo@gmail.com>
2022-10-09 14:03:04 +0700

    load2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,32 +2,47 @@ import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useState,useEffect } from "react";
-import { useRouter } from 'next/router';
+
 
 import Aos from "aos";
 import "aos/dist/aos.css";
-import Loader  from '../components/Loader'
+/*import Loader  from '../pages/Loader'*/
+
 
 
 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(false)
+
+  /*const [loading, setLoading] = useState(false)
   const router = useRouter();
+ 
 
   useEffect(() => {
   router.events.on("routeChangeStart",(url:any)=>{
     console.log("Router is changing....");
+    console.log(url);
     setLoading(true);
+    NProgress.start();
+    
   });
   router.events.on("routeChangeComplete",(url:any)=>{
     console.log("Router is changing is complete");
+    console.log(url);
     setLoading(false);
+    NProgress.done();
+   
   });
-})
+  router.events.on("routeChangeError", (err,url:any)=>{
+    console.log("Router is changing is Error");
+    setLoading(false);
+    NProgress.done();
 
+  }
+  )
   
+})*/
 
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
@@ -43,9 +58,8 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 
   return(   <>
-            {loading && <Loader/>}
             <Component {...pageProps} />
-          </>
+            </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,220 +1,39 @@
-import React,{ useState } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
-import Image from 'next/image'
-import { Nav, Button,Card,Row,Col, } from 'react-bootstrap';
-
-import Personal from './Personal'
-import Ex  from './Ex'
-import Foot from './Foot'
-
-import { resolve } from 'path';
-
-
+import React from 'react'
+import { useRouter } from 'next/router';
+import { useState,useEffect } from "react";
 type Props = {}
 
-export default function index3({}: Props) {
+export default function Loader({}: Props) {
+  const router = useRouter();
 
- 
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('Hello, World!')
+      router.push("/index2")
+    }, 2500);
+  })
+  
   return (
-
     <div>
-      <Head>
-       <title>My page title</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-
-       <Nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-          <div className="container-xl">
-            <Link href="#">
-              <a className="navbar-brand">HOME</a>
-            </Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarsExample07XL">
-              <ul className="navbar-nav mr-auto">
-               
-                <li className="nav-item ms-3">
-                  <Link href="#myskills">
-                    <a className="nav-link">My Skills</a>
-                  </Link>
-                </li>
-                <li className="nav-item ms-3">
-                  <Link href="#Myself">
-                    <a className="nav-link">Myself</a>
-                  </Link>
-                </li>
-
-                <li className="nav-item ms-3">
-                  <Link href="#Ex">
-                    <a className="nav-link">Experience</a>
-                  </Link>
-                </li>
-                <li className="nav-item ms-3">
-                  <Link href="#Contact">
-                    <a className="nav-link">Contact</a>
-                  </Link>
-                </li>
-                
-
-              </ul>
-
-            </div>
-          </div>
-      </Nav>
-
-      <section className='AA'>
-        <div className='container-fluid Me'>
-        
-        <h2 id='show_2' >
-                 Phakaphol 
-                 <span>Phakaphol </span>
-                 <span>Phakaphol </span>
-                 <span>Robotic Eng. & Web Developer</span> 
-             </h2>
-        </div>
-         
-        
-      </section>
-    
-      <div className='step pt-1' id='myskills'>
-
-        
-
-        <div className='row  pb-2'>
-          <div className='col-12 step2 d-flex justify-content-center'>
-            <h1 className='text-white myself2  mt-5  text-align: center;'>My Skills</h1>
+        <div id="loader">
+            <div id="shadow"></div>
+            <div id="box"></div>
             
-          </div>
-          
-
-          <div className='col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4 border p-5'>
-            <div className="d-flex justify-content-center">
-            <Image 
-                className="d-flex justify-content-center" 
-                src="/img/programming.png"
-                width={180}
-                height={150}
-                layout="intrinsic"
-                alt=""
-                data-aos="flip-right"
-                priority={true}
-              />
-            </div>
-          
-              
-            <h1 className='text-white d-flex justify-content-center '>Software Dev<br></br></h1>
-            
-            <div className='row '>
-
-            <div className='col'>
-            <ul className='text-white '>
-              <li className='d-flex justify-content-center'>Frameworks : Nodejs , React , Nextjs ,Bootstrap Zustand ReduxToolkit </li>
-              <li className='d-flex justify-content-center'>Program : Javascript , Typescript , HTML ,CSS , Python ,C/C++ </li>
-              <li className='d-flex justify-content-center'>ORM : Sequlize</li>
-              
-            </ul>
-
-            </div>
-
-           
-          </div>
-          
-           
         </div>
 
-          <div className='col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4 border p-5'>
-          <div className="d-flex justify-content-center">
-            <Image 
-                className="d-flex justify-content-center" 
-                src="/img/cad.png"
-                width={150}
-                height={150}
-                layout="intrinsic"
-                alt=""
-                data-aos="flip-right"
-                priority={true}
-              />
-            </div>
-          <h1 className='text-white d-flex justify-content-center mb-3 mt-3' >Design model 2D/3D</h1>
-
-          <ul className='text-white '>
-              <li className='d-flex justify-content-center'>Solidwork 2D/3D</li>
-              <li className='d-flex justify-content-center'>Simulator : Force , fluid-flow , Animation</li>
-              <li className='d-flex justify-content-center'>Circuit Wizard</li>
-          </ul>
-         
-
-          </div>
-
-          <div className='col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4 border p-5'>
-
-          <div className="d-flex justify-content-center">
-            <Image 
-                className="d-flex justify-content-center" 
-                src="/img/coaching.png"
-                width={180}
-                height={150}
-                layout="intrinsic"
-                alt=""
-                data-aos="flip-right"
-                priority={true}
-              />
-            </div>
-
-          <h1 className='text-white d-flex justify-content-center mt-3 ms-4 mb-3'>Teaching</h1>
-            <ul className='text-white'>
-                <li className='d-flex justify-content-center'>Robotic&Programing tutor</li>
-                <li className='d-flex justify-content-center'>(2016-2022)</li>
-            </ul>
- 
-          </div>
-
-        </div>
-        
-        
-       
-     
-
-      </div>
-
-      <Personal/>
-      <Ex/>
-      <Foot/>
-      
-
-      <section>
-
-      </section>
-
-
-           
 
     </div>
-    
-   
-
-    
-
-      
-       
-      
-
-
-
- 
   )
 }
 
-/*export async function getServerSideProps(){
-      await new Promise((resolve)=>{
-        setTimeout(resolve,10000);
-      });
-      return{
-        props:{},
-      };
-}
-*/
 
+
+/*export async function getServerSideProps(){
+    await new Promise((resolve)=>{
+      setTimeout(resolve,5000);
+      
+    });
+    return{
+      props:{},
+    };
+}*/
